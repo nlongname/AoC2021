24213aecf1314f655eaa786af4239b2488953d7e
Nate Langhinrichs <nate.longname@gmail.com>
2021-12-21 00:53:28 -0500

    slight optimization on pt2, didn't affect performance noticeably

--- Day21/21_2.py
+++ Day21/21_2.py
@@ -5,7 +5,8 @@
 #(at least until the last step)
 
 in_progress = {(2,1,0,0,0):1} #(position0, position1, score0, score1,turn):number of universes
-done = {}
+one_wins = 0
+two_wins = 0
 
 while in_progress != {}:
     current = list(in_progress.keys())[0]
@@ -21,18 +22,18 @@ while in_progress != {}:
                 turn = 1-turn
                 new_state = (positions[0],positions[1],scores[0],scores[1],turn)
                 if max(scores) >= 21:
-                    if new_state in done:
-                        done[new_state] += count
+                    if scores[0] > scores[1]:
+                        one_wins += count
                     else:
-                        done[new_state] = count
+                        two_wins += count
                 elif new_state in in_progress:
                     in_progress[new_state] += count
                 else:
                     in_progress[new_state] = count
     del in_progress[current]
 
-one_wins = sum([done[u] for u in done if u[2]>u[3]])
-two_wins = sum([done[u] for u in done if u[3]>u[2]])
+#one_wins = sum([done[u] for u in done if u[2]>u[3]])
+#two_wins = sum([done[u] for u in done if u[3]>u[2]])
 if one_wins > two_wins:
     print(f"one won {one_wins} times")
 else:
